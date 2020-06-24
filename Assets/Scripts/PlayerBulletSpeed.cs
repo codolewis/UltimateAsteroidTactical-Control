269d87e57806dcc341fc3824b61e9986e253886b
CODY LEWIS <codolewis@yahoo.com>
2020-06-24 18:59:29 -0500

    Added the ability to shoot
    Added the ability to shoot using the bullet prefab and the PlayerShoot script.

---
+++ Assets/Scripts/PlayerBulletSpeed.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBulletSpeed : MonoBehaviour
+{
+    public float BulletSpeed = 0; // The variable that holds the speed component
+    public Transform tf; // The variable that holds the Transform component
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = GetComponent<Transform>();  // tf will get the component Transform
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position += transform.right * BulletSpeed * Time.deltaTime;
+          tf.position = tf.position + (tf.up * BulletSpeed);
+    }
+}
---
+++ Assets/Scripts/PlayerShootScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShootScript : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    void Shoot ()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
